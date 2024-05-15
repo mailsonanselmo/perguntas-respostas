d6d64712f0dd21ce3ed6c41b42543b81bb3cca50
mailson <mailson.anselmo@gmail.com>
2024-05-14 23:32:38 -0300

    melhorando a listagem, preparando para buscar por id

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ app.use(bodyParser.json());
 
 app.get("/",(req, res) => {
 
-    Pergunta.findAll({ raw:true }).then(perguntas =>{
+    Pergunta.findAll({ raw:true, order:[
+        ['id','DESC']
+    ] }).then(perguntas =>{
         res.render("index",{
             perguntas: perguntas
         });
